b56a534792e8f0672b07d8565f3775efa103f8a8
Haruto Watanabe <w.haru10n@gmail.com>
2023-09-06 21:41:26 +0900

    modify

--- face_recognition.py
+++ face_recognition.py
@@ -5,8 +5,13 @@ from deepface import DeepFace
 import json
 from sklearn.metrics.pairwise import cosine_similarity
 
+# カメラの初期化
 cap = cv2.VideoCapture(0)
-face_net = cv2.dnn.readNetFromCaffe('deploy.prototxt', 'res10_300x300_ssd_iter_140000.caffemodel')
+
+# カスケード分類器の初期化
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# データベースの初期化
 conn = sqlite3.connect('faces.db')
 cursor = conn.cursor()
 cursor.execute('''
@@ -17,67 +22,101 @@ CREATE TABLE IF NOT EXISTS faces (
 )
 ''')
 conn.commit()
-DeepFace.build_model(model_name="VGG-Face")
-face_saved = False
-first_time_customer = False
-customer_name = ""
-recognized_customers = []
 
-def save_new_customer(face_embedding, customer_name):
-    binary_feature = json.dumps(face_embedding)
-    cursor.execute("INSERT INTO faces (name, feature) VALUES (?, ?)", (customer_name, binary_feature))
-    conn.commit()
-    print(f"新しい顧客 '{customer_name}' のデータを保存しました。")
-    recognized_customers.append(customer_name)
+# 変数の初期化
+face_detected = False
+face_included_frames = 0
+frame_threshold = 10
+customer_name = ""
+frame_count = 0
+recognition_interval = 5  # 5フレームごとに顔認識を行う
+
+# 5回連続で同一人物として判定するための変数
+last_recognized_name = ""
+continuous_recognition_count = 0
+recognition_threshold = 5
+
+
+# 名前が表示されるフレーム数を設定
+display_name_duration = 50  # 例: 50フレーム
+display_name_counter = 0
+display_name = ""
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104, 177, 123))
-    face_net.setInput(blob)
-    detections = face_net.forward()
-    for i in range(0, detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
-        if confidence > 0.5 and confidence < 1.0:
-            box = detections[0, 0, i, 3:7] * np.array([frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]])
-            (x, y, w, h) = box.astype("int")
-            # 顔の周りに四角形を描画
-            cv2.rectangle(frame, (x, y), (x + int(w * 0.3), y + int(h * 0.6)), (0, 255, 0), 1)
-            face = frame[y:y + h, x:x + w]
+
+    frame_count += 1
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in faces:
+        face = frame[y:y+h, x:x+w]
+
+        if frame_count % recognition_interval == 0:
             face_embedding = DeepFace.represent(face, model_name="VGG-Face", enforce_detection=False)
-            if first_time_customer:
-                cv2.putText(frame, "Enter Customer Name:", (10, 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
-                if customer_name == "":
-                    customer_name = input("Enter Customer Name: ")
-                else:
-                    save_new_customer(face_embedding, customer_name)
-                    first_time_customer = False
-            else:
+
+            if face_detected:
                 recognized_names = []
                 for row in cursor.execute("SELECT name, feature FROM faces"):
                     saved_name = row[0]
-                    saved_feature_list = json.loads(row[1])  # BLOBデータをJSON形式からPythonリストに変換
-                    saved_feature_dict = saved_feature_list[0]
-                    saved_feature = np.array(saved_feature_dict['embedding'], dtype=np.float32)
-                    face_embedding_dict = DeepFace.represent(face, model_name="VGG-Face", enforce_detection=False)
-                    face_embedding = np.array(face_embedding_dict[0]['embedding'], dtype=np.float32)
-                    if face_embedding.shape != saved_feature.shape:
-                        face_embedding = face_embedding.reshape(saved_feature.shape)
-                    similarity = cosine_similarity(face_embedding.reshape(1, -1), saved_feature.reshape(1, -1))
+                    saved_data = json.loads(row[1])
+                    if isinstance(saved_data, list):
+                        saved_feature = np.array(saved_data[0]['embedding'], dtype=np.float32)
+                    else:
+                        saved_feature = np.array(saved_data['embedding'], dtype=np.float32)
 
-                    # 閾値の調整
-                    if similarity[0][0] > 0.8:
-                        print(similarity[0][0])
+                    if isinstance(face_embedding, list):
+                        face_embedding_array = np.array(face_embedding[0]['embedding'], dtype=np.float32)
+                    else:
+                        face_embedding_array = np.array(face_embedding['embedding'], dtype=np.float32)
+
+                    if face_embedding_array.shape != saved_feature.shape:
+                        face_embedding_array = face_embedding_array.reshape(saved_feature.shape)
+                    similarity = cosine_similarity(face_embedding_array.reshape(1, -1), saved_feature.reshape(1, -1))
+                    if similarity[0][0] > 0.6:
                         recognized_names.append(saved_name)
-                if recognized_names:
-                    print(f"Welcome {recognized_names[0]}")
-                    cv2.putText(frame, f"Match {recognized_names[0]}", (10, 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
-                    cv2.putText(frame, f"{similarity[0][0]}", (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
-                else:
-                    print("Does not match")
-                    cv2.putText(frame, f"Does not match", (10, 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
 
+                if recognized_names:
+                    if recognized_names[0] == last_recognized_name:
+                        continuous_recognition_count += 1
+                    else:
+                        continuous_recognition_count = 1
+                        last_recognized_name = recognized_names[0]
+
+                    if continuous_recognition_count >= recognition_threshold:
+                        cv2.putText(frame, f"Welcome {last_recognized_name}", (x, y-10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
+                        print(f"Welcome {last_recognized_name}")
+                        continuous_recognition_count = 0  # カウントをリセット
+                else:
+                    continuous_recognition_count = 0  # 顔が認識されなかった場合、カウントをリセット
+                    last_recognized_name = ""
+                    if customer_name == "":
+                        customer_name = input("Enter Customer Name: ")
+                        binary_feature = json.dumps({'embedding': face_embedding[0]['embedding']})
+                        cursor.execute("INSERT INTO faces (name, feature) VALUES (?, ?)", (customer_name, binary_feature))
+                        conn.commit()
+                        print(f"新しい顧客 '{customer_name}' のデータを保存しました。")
+                        customer_name = ""
+                    face_detected = False
+                    face_included_frames = 0
+            else:
+                face_included_frames += 1
+                if face_included_frames == frame_threshold:
+                    face_detected = True
+    # 名前の表示制御をforループの外に移動
+    if continuous_recognition_count >= recognition_threshold:
+        display_name = f"Welcome {last_recognized_name}"
+        if display_name_counter == 0:  # この行を追加
+            display_name_counter = display_name_duration  # 名前の表示カウンタを設定
+        continuous_recognition_count = 0  # カウントをリセット
+
+    if display_name_counter > 0:
+        # 名前の表示カウンタが0より大きい場合、名前を表示し続ける
+        cv2.putText(frame, display_name, (10, 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
+        display_name_counter -= 1
     cv2.imshow('Camera', frame)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
