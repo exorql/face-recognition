cbf408025502fc00ddc7fff4e6d0527ccde93608
Haruto Watanabe <w.haru10n@gmail.com>
2023-09-13 15:13:33 +0900

    modify

--- face_recognition.py
+++ face_recognition.py
@@ -10,31 +10,30 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Face recognition system using Jetson Nano.')
 
-# Detector backend argument with default value set to 'MediaPipe'
-parser.add_argument('-d', '--detector_backend', type=str, default='opencv', choices=['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe', 'yolov8', 'yunet'], help='Detector backend to use. Options: opencv, ssd, dlib, mtcnn, retinaface, mediapipe, yolov8, yunet')
-
-# Model name argument with default value set to 'VGG-Face'
-parser.add_argument('-m', '--model_name', type=str, default="VGG-Face", choices=['VGG-Face', 'Facenet', 'OpenFace', 'DeepFace', 'DeepID', 'ArcFace', 'Dlib'], help="Face recognition model. Options: 'VGG-Face', 'Facenet', 'OpenFace', 'DeepFace', 'DeepID', 'ArcFace', 'Dlib'")
-
-parser.add_argument('-dm', '--distance_metric', type=str, default="euclidean", choices=['cosine', 'euclidean', 'euclidean_l2'], help="Distance metric to use for face similarity. Options: 'cosine', 'euclidean', 'euclidean_l2'")
-
-# Source argument for webcam index or video file path
+# Arguments
+parser.add_argument('-d', '--detector_backend', type=str, default='opencv', choices=['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe', 'yolov8', 'yunet'], help='Detector backend to use.')
+parser.add_argument('-m', '--model_name', type=str, default="VGG-Face", choices=['VGG-Face', 'Facenet', 'OpenFace', 'DeepFace', 'DeepID', 'ArcFace', 'Dlib'], help="Face recognition model.")
+parser.add_argument('-dm', '--distance_metric', type=str, default="euclidean", choices=['cosine', 'euclidean', 'euclidean_l2'], help="Distance metric for face similarity.")
 parser.add_argument('-s', '--source', type=str, default='0', help='Camera source index or video file path')
 
 args = parser.parse_args()
 
+# Variables from arguments
 detector_backend = args.detector_backend
 model_name = args.model_name
 distance_metric = args.distance_metric
 source = args.source
 
-model_name=args.model_name
-detector_backend = args.detector_backend
+# Open video source
 if source.isdigit():
-    cap = cv2.VideoCapture(int(source))  # Open webcam by index
+    cap = cv2.VideoCapture(int(source))
 else:
-    cap = cv2.VideoCapture(source)  # Open video file by path
+    cap = cv2.VideoCapture(source)
     fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+# Set video resolution
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 # Initialize the database
 conn = sqlite3.connect('faces.db')
@@ -54,21 +53,20 @@ face_included_frames = 0
 frame_threshold = 5
 customer_name = ""
 frame_count = 0
-recognition_interval = 5
+recognition_interval = 1  # Changed to 1 for testing
 target_size = functions.find_target_size(model_name=model_name)
-
-# Variable for determining the same person 5 times in a row
 last_recognized_name = ""
 continuous_recognition_count = 0
 recognition_threshold = 1
-current_display_name= ""
+current_display_name = ""
 freeze_start_time = None
 freeze_duration = 5
 freezed_frame = None
-
 non_matched_count = 0
 last_face_position = None
 last_name_position = None
+
+distances = []
 
 while True:
     ret, frame = cap.read()
@@ -84,7 +82,7 @@ while True:
     if frame_count % recognition_interval == 0:
       # 入力画像（frame）から顔を検出し、その顔の情報を取得
       face_objs = DeepFace.extract_faces(img_path=frame, detector_backend=detector_backend, target_size=target_size, enforce_detection=False)
-      faces = [(obj["facial_area"]["x"], obj["facial_area"]["y"], obj["facial_area"]["w"], obj["facial_area"]["h"]) for obj in face_objs if obj.get('confidence', 0) > 4]
+      faces = [(obj["facial_area"]["x"], obj["facial_area"]["y"], obj["facial_area"]["w"], obj["facial_area"]["h"]) for obj in face_objs if obj.get('confidence', 0) > 0.9]
       if freeze_start_time and time.time() - freeze_start_time > freeze_duration:
           freeze_start_time = None
           last_recognized_name = ""
@@ -98,16 +96,16 @@ while True:
           face_included_frames += 1
 
           if not freeze_start_time:
-              try:
-                  # 指定されたモデル（model_name）を使用して、入力された顔画像（face）の特徴ベクトル（embedding）を取得
-                  face_embedding = DeepFace.represent(face, model_name=model_name, enforce_detection=True)
-              except ValueError:
-                  print("Face not detected.")
-                  continue
+              # 指定されたモデル（model_name）を使用して、入力された顔画像（face）の特徴ベクトル（embedding）を取得
+              face_embedding = DeepFace.represent(face, model_name=model_name, enforce_detection=False)
+
               if face_detected and face_embedding:
                   recognized_names = []
+                  distances = []
+                  saved_names = []
                   for row in cursor.execute("SELECT name, feature FROM faces"):
                       saved_name = row[0]
+                      saved_names.append(saved_name)
                       saved_data = json.loads(row[1])
                       if isinstance(saved_data, list):
                           saved_feature = np.array(saved_data[0]['embedding'], dtype=np.float32)
@@ -121,6 +119,7 @@ while True:
 
                       if face_embedding_array.shape != saved_feature.shape:
                           face_embedding_array = face_embedding_array.reshape(saved_feature.shape)
+
                       if distance_metric == "cosine":
                           similarity = 1 - dst.findCosineDistance(face_embedding_array, saved_feature)
                       elif distance_metric == "euclidean":
@@ -128,12 +127,21 @@ while True:
                       elif distance_metric == "euclidean_l2":
                           similarity = -dst.findEuclideanDistance(dst.l2_normalize(face_embedding_array), dst.l2_normalize(saved_feature))
 
-                      threshold = dst.findThreshold(model_name, distance_metric)
-
                       print(f"{similarity}")
+                      distances.append(similarity)
 
-                      if similarity > threshold:
-                          recognized_names.append(saved_name)
+                  if distances:
+                      min_distance = min(distances)
+                      best_face_index = np.argmin(distances)
+                      if best_face_index < len(faces):  # この行を追加
+                          best_face_area = faces[best_face_index]
+                      threshold = dst.findThreshold(model_name, distance_metric)
+                      print(f"最も類似性の高い距離: {min_distance}")
+                      best_similarity = distances[best_face_index]
+                      print(f"{best_similarity}")
+                      if similarity < threshold:
+                          recognized_name = saved_names[best_face_index]
+                          recognized_names.append(recognized_name)
 
                   if recognized_names:
                       if recognized_names[0] == last_recognized_name:
