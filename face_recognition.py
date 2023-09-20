b439b86c62bd9f2798bab4ec9caa2667ac8c6cc5
Haruto Watanabe <w.haru10n@gmail.com>
2023-09-20 21:31:34 +0900

    modify

--- face_recognition.py
+++ face_recognition.py
@@ -7,13 +7,18 @@ import json
 from deepface.commons import functions
 from commons import distance as dst
 import argparse
+import mediapipe as mp
+
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(max_num_faces=1)
+mp_drawing = mp.solutions.drawing_utils
 
 parser = argparse.ArgumentParser(description='Face recognition system using Jetson Nano.')
 
 # Arguments
-parser.add_argument('-d', '--detector_backend', type=str, default='opencv', choices=['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe', 'yolov8', 'yunet'], help='Detector backend to use.')
-parser.add_argument('-m', '--model_name', type=str, default="VGG-Face", choices=['VGG-Face', 'Facenet', 'OpenFace', 'DeepFace', 'DeepID', 'ArcFace', 'Dlib'], help="Face recognition model.")
-parser.add_argument('-dm', '--distance_metric', type=str, default="euclidean", choices=['cosine', 'euclidean', 'euclidean_l2'], help="Distance metric for face similarity.")
+parser.add_argument('-d', '--detector_backend', type=str, default='mediapipe', choices=['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe', 'yolov8', 'yunet'], help='Detector backend to use.')
+parser.add_argument('-m', '--model_name', type=str, default="Facenet", choices=['VGG-Face', 'Facenet', 'OpenFace', 'DeepFace', 'DeepID', 'ArcFace', 'Dlib'], help="Face recognition model.")
+parser.add_argument('-dm', '--distance_metric', type=str, default="cosine", choices=['cosine', 'euclidean', 'euclidean_l2'], help="Distance metric for face similarity.")
 parser.add_argument('-s', '--source', type=str, default='0', help='Camera source index or video file path')
 
 args = parser.parse_args()
@@ -41,7 +46,7 @@ cursor = conn.cursor()
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS faces (
     id INTEGER PRIMARY KEY,
-    name TEXT,
+    number INTEGER,
     feature BLOB
 )
 ''')
@@ -65,6 +70,7 @@ freezed_frame = None
 non_matched_count = 0
 last_face_position = None
 last_name_position = None
+new_sequence = 0
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
@@ -76,11 +82,18 @@ while True:
     if frame is None:
         break
 
+    # mediapipeを使用して顔のランドマークを検出
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = face_mesh.process(image_rgb)
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            nose_tip = np.array([[landmark.x, landmark.y] for landmark in face_landmarks.landmark if landmark.z < -0.5])
+            if len(nose_tip) > 0:
+                continue
     face_embedding = None
     frame_count += 1
 
     if frame_count % recognition_interval == 0:
-      # 入力画像（frame）から顔を検出し、その顔の情報を取得
       face_objs = DeepFace.extract_faces(img_path=frame, detector_backend=detector_backend, target_size=target_size, enforce_detection=False)
       faces = [(obj["facial_area"]["x"], obj["facial_area"]["y"], obj["facial_area"]["w"], obj["facial_area"]["h"]) for obj in face_objs if obj.get('confidence', 0) > 0.9]
       if freeze_start_time and time.time() - freeze_start_time > freeze_duration:
@@ -103,7 +116,7 @@ while True:
                   recognized_names = []
                   distances = []
                   saved_names = []
-                  for row in cursor.execute("SELECT name, feature FROM faces"):
+                  for row in cursor.execute("SELECT number, feature FROM faces"):
                       saved_name = row[0]
                       saved_names.append(saved_name)
                       saved_data = json.loads(row[1])
@@ -127,19 +140,17 @@ while True:
                       elif distance_metric == "euclidean_l2":
                           similarity = -dst.findEuclideanDistance(dst.l2_normalize(face_embedding_array), dst.l2_normalize(saved_feature))
 
-                      print(f"{similarity}")
                       distances.append(similarity)
 
                   if distances:
-                      min_distance = min(distances)
-                      best_face_index = np.argmin(distances)
-                      if best_face_index < len(faces):  # この行を追加
+                      max_similarity = max(distances)
+                      best_face_index = np.argmax(distances)
+                      if best_face_index < len(faces):
                           best_face_area = faces[best_face_index]
                       threshold = dst.findThreshold(model_name, distance_metric)
-                      print(f"最も類似性の高い距離: {min_distance}")
-                      best_similarity = distances[best_face_index]
-                      print(f"{best_similarity}")
-                      if similarity < threshold:
+                      print(f"最も類似性の高い距離: {max_similarity}")
+                      best_similarity = max_similarity
+                      if best_similarity > threshold:
                           recognized_name = saved_names[best_face_index]
                           recognized_names.append(recognized_name)
 
@@ -162,13 +173,19 @@ while True:
                       last_recognized_name = ""
 
                       non_matched_count += 1
-                      if non_matched_count >= 3 and face_detected:
+                      if non_matched_count >= 5 and face_detected:
                           if face_embedding:
-                              customer_name = input("Enter Customer Name: ")
+                              cursor.execute("SELECT MAX(number) FROM faces")
+                              max_sequence = cursor.fetchone()[0]
+                              if max_sequence is None:
+                                  max_sequence = 0
+
+                              new_sequence = max_sequence + 1
+
                               binary_feature = json.dumps(face_embedding)
-                              cursor.execute("INSERT INTO faces (name, feature) VALUES (?, ?)", (customer_name, binary_feature))
+                              cursor.execute("INSERT INTO faces (number, feature) VALUES (?, ?)", (new_sequence, binary_feature))
                               conn.commit()
-                              print(f"新しい顧客 '{customer_name}' のデータを保存しました。")
+                              print(f"新しい顔のデータを保存しました。連番: {new_sequence}")
                           face_detected = False
                           face_included_frames = 0
                           non_matched_count = 0
